cb8da3b1a83059bf0a4bc5e8046aa8a50c5534a1
Eduardo-Bocon <eduardo.h.p.bocon@gmail.com>
2024-09-08 22:59:26 -0300

    tests api

--- src/app/api/receiveESP32data.tsx
+++ src/app/api/receiveESP32data.tsx
@@ -5,6 +5,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     
     const { status, valor_sensor } = req.body;
 
+    console.log("Deu bom caralho!")
+    console.log(req.body)
+
     res.status(200).json({
       message: 'Dados recebidos com sucesso!',
       status: status,
