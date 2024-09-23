17147e514f4ea04c5c15daf18760355eeed1f0c6
Eduardo Boçon <78614827+Eduardo-Bocon@users.noreply.github.com>
2024-09-23 18:43:31 -0300

    landing page improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-6xl ">Robonize</h1>
-        <h2>The future needs machine tokenization</h2>
-        <a href="/washingMachine">
-          <button className="bg-green-600 p-3 rounded-xl">
-            Washing Machine Test
-          </button>
-        </a>
+    <div className="bg-slate-950 h-screen">
+      <main className="flex flex-col gap-8 row-start-2 sm:items-start h-full">
+        <header className="bg-slate-800 h-24 w-full">
+          <a href="/">Robonize</a>
+        </header>
+        <div className="flex flex-col justify-center h-full w-screen">
+          <h1 className="text-9xl flex justify-center">Robonize</h1>
+          <a href="/washingMachine" className="flex justify-center py-10">
+            <button className="bg-[#259776] py-5 px-9 rounded-3xl text-6xl">
+              Launch
+            </button>
+          </a>
+        </div>
+        
       </main>
       
     </div>
