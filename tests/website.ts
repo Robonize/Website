d58f546c0e343fac2a39a6a1168bd91ace569e04
Eduardo Boçon <78614827+Eduardo-Bocon@users.noreply.github.com>
2024-11-10 19:43:53 -0300

    quick fixes

--- tests/website.ts
+++ tests/website.ts
@@ -1,12 +1,12 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
-import { Robonize } from "../target/types/robonize";
+import { Website } from "../target/types/website";
 
 describe("robonize", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.Robonize as Program<Robonize>;
+  const program = anchor.workspace.Robonize as Program<Website>;
 
   it("Is initialized!", async () => {
     // Add your test here.
